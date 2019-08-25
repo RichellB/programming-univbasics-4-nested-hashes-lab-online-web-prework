7086a7f767b34857254abf220b8000bc3762860f
Richell Bonfield <richell.bonfield12@gmail.com>
2019-08-25 01:20:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -72,7 +72,13 @@ def adding_matz
 # }
   #  programmer_hash[:yukihiro_matsumoto] = {:
 	programmer_hash = 
- 		{
+ 		{ 
+ 		    :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+        
+ 		    },
+ 		    
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -108,6 +114,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
      
 end
